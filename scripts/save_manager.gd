23f2734fe4bb17c37ab24fee5364f94f7f4a071c
Claire Woods <sam@lizandler.com>
2024-02-17 13:54:52 -0600

    added loading levels with game scene as overhead

--- scripts/save_manager.gd
+++ scripts/save_manager.gd
@@ -1,26 +1,34 @@
 class_name SaveManager
 
-const LEVEL_0 : String = "res://scenes/game.tscn"
+const default_data : Dictionary = { "hub_level_path" : "res://scenes/test_hub.tscn",
+									"level_path_array" : ["res://scenes/test_board.tscn"],
+									"current_level_index" : 0, # If negitive, send to hub instead
+									"display_name" : "Test Game",
+									"achivement_flags" : 0 # This is entirely overkill
+									}
+
+const hub_prefab : PackedScene = preload("res://scenes/test_hub.tscn")
+const board_prefab : PackedScene = preload("res://scenes/game.tscn")
 
 static var save_location = 0
 
+static func get_save_header(index : int) -> Dictionary:
+	var data := get_save_data(index)
+	return {"display_name" : data["display_name"], "achivement_flags" : data["achivement_flags"]}
+
+static func achivement_flags_to_completion_percent(flags : int) -> float:
+	return 0
+
 static func load_save(index : int, tree : SceneTree):
-	var data : Dictionary = {"Level" : LEVEL_0, "Checkpoint" : 0}
-	if FileAccess.file_exists("user://savegame_%s.save" % index):
-		var save_game = FileAccess.open("user://savegame_%s.save" % index, FileAccess.READ)
-		var json_string = save_game.get_line()
-
-		# Creates the helper class to interact with JSON
-		var json = JSON.new()
-
-		# Check if there is any error while parsing the JSON string, skip in case of failure
-		var parse_result = json.parse(json_string)
-		if not parse_result == OK:
-			print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
-			return
-		
-		data = json.get_data()
+	var data = get_save_data(index)
 	
+	await close_current_tree(tree)
+	
+	await switch_tree(tree,data)
+	
+	await open_new_tree(tree, data)
+
+static func close_current_tree(tree : SceneTree):
 	var pre_level_animatior : AnimationPlayer = tree.current_scene.get_node_or_null("Animations")
 	if pre_level_animatior and pre_level_animatior.has_animation("Outro"):
 		pre_level_animatior.play("Outro")
@@ -29,21 +37,29 @@ static func load_save(index : int, tree : SceneTree):
 	var level_transitioner := LevelTransition.get_node("AnimationPlayer") as AnimationPlayer
 	level_transitioner.play("transition")
 	await  level_transitioner.animation_finished
+
+static func switch_tree(tree : SceneTree, data : Dictionary):
+	if data["current_level_index"] < 0:
+		var err = tree.change_scene_to_file(data["hub_level_path"])
+		if not err == OK:
+			print("Scene failed to load with error ", error_string(err))
+		
+		await tree.tree_changed
+	else:
+		var err = tree.change_scene_to_packed(board_prefab)
+		if not err == OK:
+			print("Scene failed to load with error ", error_string(err))
+		
+		var board_prefab : PackedScene = load(data["level_path_array"][data["current_level_index"]])
+		
+		await tree.tree_changed
+		
+		tree.current_scene.add_child(board_prefab.instantiate())
+
+static func open_new_tree(tree : SceneTree, data : Dictionary):
+	apply_saved_level_data(tree,data)
 	
-	var err = tree.change_scene_to_file(data["Level"])
-	if not err == OK:
-		print("Scene failed to load with error ", error_string(err))
-		return
-	
-	await tree.tree_changed
-	
-	var checkpoint_parent = tree.current_scene.get_node_or_null("CatCheckpoints")
-	if checkpoint_parent:
-		var checkpoint = checkpoint_parent.get_child(data["Checkpoint"])
-		var player_character = tree.current_scene.get_node_or_null("PlayerCat")
-		player_character.position = checkpoint.to_global(checkpoint.respawn_point)
-		player_character.checkpoint = checkpoint
-	
+	var level_transitioner := LevelTransition.get_node("AnimationPlayer") as AnimationPlayer
 	level_transitioner.play_backwards("transition")
 	await level_transitioner.animation_finished
 	
@@ -52,7 +68,29 @@ static func load_save(index : int, tree : SceneTree):
 		post_level_animatior.play("Intro")
 		await post_level_animatior.animation_finished
 
-static func save(index : int, level : StringName, checkpoint : int):
+static func apply_saved_level_data(tree : SceneTree, data : Dictionary):
+	pass
+
+static func get_save_data(index : int) -> Dictionary:
+	var data := default_data
+	if not FileAccess.file_exists("user://savegame_%s.save" % index):
+		return data
+	
+	var save_game = FileAccess.open("user://savegame_%s.save" % index, FileAccess.READ)
+	var json_string = save_game.get_as_text()
+	
+	# Creates the helper class to interact with JSON
+	var json = JSON.new()
+	
+	# Check if there is any error while parsing the JSON string, skip in case of failure
+	var parse_result = json.parse(json_string)
+	if not parse_result == OK:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+		return data
+	
+	return json.get_data()
+
+static func save(index : int, changes : Dictionary):
+	var data = get_save_data(index)
 	var save_game = FileAccess.open("user://savegame_%s.save" % index,FileAccess.WRITE)
-	var data = {"Level" : level, "Checkpoint" : checkpoint}
-	save_game.store_line(JSON.stringify(data))
+	save_game.store_line(JSON.stringify(data,"\t"))
